cdc011aa1a8d5cf618e3d9a016a30d7d84e803c9
Кирилл <kattoshkin@gmail.com>
2021-07-17 16:55:27 +0700

    Добавьте файлы проекта.

---
+++ oop1_AnimalSample_CreatingClass/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1_AnimalSample_CreatingClass
+{
+    class Animal
+    {
+        //поля
+        public string name;    //кличка
+        public float mass = 0; //вес
+        public char color;     //цвет
+
+        //конструкторы
+        public Animal() { }
+        public Animal(string name1, float mass1, char color1)
+        {
+            name = name1;
+            mass = mass1;
+            color = color1;
+        }
+        public Animal(string name, float mass)
+        {
+            this.name = name;
+            this.mass = mass;
+        }
+
+        //методы
+        public virtual void say()
+        {
+            Console.WriteLine($"{name} ({color}, {mass}) says:");
+            Console.WriteLine("Abstract voice"); //издать абстрактный звук
+        }
+    }
+}
---
+++ oop1_AnimalSample_CreatingClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop1_AnimalSample_CreatingClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
